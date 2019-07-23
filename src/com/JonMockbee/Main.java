f24c673da736cd098cdf55bec3e0bd612b626289
Jon Mockbee <mockbee1000@gmail.com>
2019-07-23 16:53:22 -0400

    reverse for loop

--- src/com/JonMockbee/Main.java
+++ src/com/JonMockbee/Main.java
@@ -3,11 +3,18 @@ package com.JonMockbee;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("10,0000 at 2% interest" + calculateInterest(10000,2.0));
+        System.out.println("10,0000 at 2% interest " + calculateInterest(10000,2.0));
     for (int i= 0; i<5; i++){
         System.out.println("Loop " + i + " hello!");
     }
+        for (int i = 2; i<9; i++){
+            System.out.println("10,0000 at"+ i + "% interest " + String.format("%.2f",calculateInterest(10000,i)));
+        }
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 
+        for (int i = 8; i >=2; i--){
+            System.out.println("10,0000 at"+ i + "% interest " + String.format("%.2f",calculateInterest(10000,i)));
+        }
 
     }
 
